59a6212a6cecc1b931587f69a1c5bbf72419f91b
Dan Bellamy <bdanbellamy@gmail.com>
2022-10-04 11:52:08 +0200

    auto-commit-792430e5643b44e1849d9e858153eee3

--- next.config.js
+++ next.config.js
@@ -10,6 +10,6 @@ module.exports = withFrameworkConfig({
     locales:["en-US","es"],
     defaultLocale:"en-US"
   }
-})
+  })
 
 console.log("next.config.js",JSON.stringify(module.exports),null,2);
