d000088e8fc194ab208a646bdb41aea94794bb94
Dan Bellamy <bdanbellamy@gmail.com>
2022-10-04 11:37:09 +0200

    auto-commit-168ecfdfdcfa46069ce580ec96494b6f

--- framework/common/config.js
+++ framework/common/config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 
 function withFrameworkConfig(defaultConfig={}){
-    const framework = "shopgiy";
+    const framework = "shopify";
+
+    const frameworkNextConfig = path.join("../",framework/next.config.js");
 }
