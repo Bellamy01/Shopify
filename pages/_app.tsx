4508d878cfad04db37e8c8ad078e674050ec7f4f
Dan Bellamy <bdanbellamy@gmail.com>
2022-10-05 21:16:49 +0200

    auto-commit-8b54571fd43a4c2ea766172581dcebae

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,19 @@
+import "@assets/main.css";
 import { AppProps } from "next/app";
 import { FC } from "react";
 import { Layout } from "@components/common";
+
 
 const Noop : FC = ({children}) => <>{children}</>
 
 function MyApp({Component, pageProps}:AppProps & {Component:{Layout: FC}}){
 
   const Layout = Component.Layout ?? Noop;
+ 
   return (
-   
       <Layout>
         <Component  {...pageProps}/>
       </Layout>
-    
   )
 }
 
