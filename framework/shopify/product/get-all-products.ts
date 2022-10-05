4322df4a0daf3fe7bcc02ac8a2b3aef01ae9ffb0
Dan Bellamy <bdanbellamy@gmail.com>
2022-10-05 11:58:58 +0200

    auto-commit-6d52f664edf440449214a88180c74a80

--- framework/shopify/product/get-all-products.ts
+++ framework/shopify/product/get-all-products.ts
@@ -3,14 +3,16 @@ import {fetchApi} from "../utils";
 import { ProductConnection } from "../schema";
 import { normalizeProduct} from "../utils/normalize";
 import { Product } from "@common/types/product";
-
+import {ApiConfig} from ""
 
 type ReturnType ={
     products: ProductConnection
 }
 
 const getAllProducts = async ():Promise<Product[]>=>{
-    const {data} = await fetchApi<ReturnType>({query:getAllProductQuery});
+    const {data} = await fetchApi<ReturnType>({
+        url:ApiConfig,
+        query:getAllProductQuery});
     //normalize and return new data!
     const products = data.products.edges.map(({node : product})=>
         normalizeProduct(product)
