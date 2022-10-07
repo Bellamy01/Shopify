184708b8581c16511a729bfac6878c239e19e2ee
Dan Bellamy <bdanbellamy@gmail.com>
2022-10-07 11:45:08 +0200

    auto-commit-5545559e97c64341850064874aac7606

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+
+
+  module.exports = {
+  darkMode:'class',
+  content: [
+    './pages/**/*.{js,ts,tsx,jsx}',
+    './components/**/*.{js,ts,tsx,jsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: [],
+}
