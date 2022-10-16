57aae0d7d332529cd66fd0623da33288277132ef
Dan Bellamy <bdanbellamy@gmail.com>
2022-10-16 09:00:34 +0200

    auto-commit-ce784c3cf5214358a29d078d19686383

--- components/common/ui/Marquee/Marquee.tsx
+++ components/common/ui/Marquee/Marquee.tsx
@@ -1,11 +1,17 @@
 import { ReactNode, FC } from "react";
 import s from "./Marquee.module.scss";
+import cn from "classnames";
 
 interface Props {
-    children: ReactNode[]
+    children: ReactNode[],
+    variant: "primary" | "secondary"
 }
 
-const Marquee : FC<Props> = ({children})=>{
+const Marquee : FC<Props> = ({children,variant = "primary"})=>{
+    const rootClassName = cn(
+        s.root,
+        {}
+    )
     return (
         <div className={s.root}>
             <div className={s.loop}>
