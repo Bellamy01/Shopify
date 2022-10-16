a85180789ea27833e14a84ad226ebb53296e0b0e
Dan Bellamy <bdanbellamy@gmail.com>
2022-10-16 12:33:39 +0200

    auto-commit-515a6fcc9e7144468f3ba599fc044d8a

--- components/common/ui/Context.tsx
+++ components/common/ui/Context.tsx
@@ -1,6 +1,23 @@
-import { FC } from "react";
+import { FC,ReactNode, useContext } from "react";
 import { createContext } from "vm";
 
-const UIContext =  createContext
+interface Props {
+    children:ReactNode[]
+}
+const UIContext =  createContext({
+    uiState:"defaultState"
+})
 
-export const UIProvider = ({children})=>
+
+export const UIProvider:FC<Props> = ({children})=>{
+    return(
+        <UIContext.Provider value={{uiState:"someState"}}>
+            {children}
+        </UIContext.Provider>
+    )
+}
+
+export const useUI = ()=>{
+    const context = useContext(UIContext);
+    return context;
+}
