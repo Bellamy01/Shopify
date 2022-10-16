ae1749eff66a75fd5b2cf932c702d7d08242294e
Dan Bellamy <bdanbellamy@gmail.com>
2022-10-16 12:13:24 +0200

    auto-commit-eaed6dd68b5d4e859ec2b2b4ec80c88f

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -3,6 +3,7 @@ import { FC, ReactNode } from "react";
 import s from "./Layout.module.css";
 import {Footer} from "@components/common";
 import {NavBar} from "@components/common";
+import {SideBar} from "@components/common";
 interface Props {
     children: ReactNode[]
 }
@@ -10,6 +11,9 @@ const Layout: FC<Props> = ({children})=>{
     return (
         <div className={s.root}>
         <NavBar/>
+        <SideBar>
+            
+        </SideBar>
         <main className="fit">
             { children }
         </main>
