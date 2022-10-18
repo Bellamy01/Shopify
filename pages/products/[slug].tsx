dc45cf70078b7a2482e76b9570abbb5c50bf3bd5
Dan Bellamy <bdanbellamy@gmail.com>
2022-10-18 05:35:37 +0200

    auto-commit-8515e9477e2549c481712e6364414d10

--- pages/products/[slug].tsx
+++ pages/products/[slug].tsx
@@ -1,16 +1,31 @@
 import { Layout } from "@components/common";
+import { GetStaticPaths, GetStaticPropsContext, InferGetStaticPropsType } from "next";
 
-export const getStaticPaths = ()=>{
-
+//fecth all of the product slugs
+export const getStaticPaths : GetStaticPaths = async()=>{
+    return {
+        paths:[
+            {params:{slug:"cool-hat"}},
+            {params:{slug:"t-shirt"}},
+            {params:{slug:"lightweight-jacket"}}
+        ],
+        fallback: false
+    }
 }
-
-
-export default function ProductSlug(){
-
-
+//provide specific data to the page
+export const getStaticProps = async({params}:GetStaticPropsContext<{slug:string}>)=>{
+    return{
+        props:{
+            product:{
+                slug: params?.slug
+            }
+        }
+    }
+}
+export default function ProductSlug({product}:InferGetStaticPropsType<typeof getStaticProps>){
     return(
         <div>
-            Hello there
+            {product.slug}
         </div>
     )
 }
