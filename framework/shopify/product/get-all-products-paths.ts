7cbbdb195400baea43fcf7cb66967cc19d67eef1
Dan Bellamy <bdanbellamy@gmail.com>
2022-10-18 07:06:15 +0200

    auto-commit-1e9b032d1f6c4f6eb2e070f37cb3fd7f

--- framework/shopify/product/get-all-products-paths.ts
+++ framework/shopify/product/get-all-products-paths.ts
@@ -1,10 +1,16 @@
 import { ApiConfig } from "@common/types/api";
 import { Product } from "@common/types/product";
+import getAllProductPathsQuery from "@framework/utils/queries/get-all-products-paths";
 
 type ReturnType = {
     products: Pick<Product,"slug">[]
 }
 const  getAllProductsPaths = async(config:ApiConfig):Promise<ReturnType> =>{
+
+    const {data} = config.fetch({
+        query:getAllProductPathsQuery,
+        url:config.apiUrl
+    });
     return {
         products:[
             {slug:"cool-hat"},
--- framework/shopify/utils/queries/get-all-products-paths.ts
+++ framework/shopify/utils/queries/get-all-products-paths.ts
@@ -1,7 +1,15 @@
 import { GetAllProductPathsQuery } from "@framework/schema";
 
 const getAllProductQuery = `
-
+    query getAllProductPaths($first: Int = 250){
+        products(first:$first){
+            edges{
+                node{
+                    handle
+                }
+            }
+        }
+    }
 `
 
 
